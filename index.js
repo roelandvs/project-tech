8095d0a2c1a488a82a5d0a8e15f387e67a7a3fbd
Roeland <roelandsteevan@gmail.com>
2020-05-10 16:29:39 +0200

    serve assignment

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//link to the static file directory
+app.use('/static', express.static('static'))
 
+//homepage that uses an html file
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+
+//contact page that just contains text
 app.get('/contact', (req, res) => res.send('This is the contact page!'))
+
+//about page that just contains text
+app.get('/about', (req, res) => res.send('This is the about page!'));
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
