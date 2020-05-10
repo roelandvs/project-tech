4ed38c4eed464793c3ac6ae7a26347f5ec35f94d
Roeland <roelandsteevan@gmail.com>
2020-05-10 15:46:57 +0200

    index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/contact', (req, res) => res.send('This is the contact page!'))
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
