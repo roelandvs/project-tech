f15efa45f00cb1cce7817258a794d711e2df3ceb
Roeland <roelandsteevan@gmail.com>
2020-05-11 11:41:53 +0200

    add 404 page messgage

--- index.js
+++ index.js
@@ -14,5 +14,10 @@ app.get('/contact', (req, res) => res.send('This is the contact page!'))
 //about page that just contains text
 app.get('/about', (req, res) => res.send('This is the about page!'));
 
+//handles 404 pages
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry, niet deze pagina bestaat niet vriend")
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
