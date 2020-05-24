0001ea4fdebe15cb9d6a68e2bd97378a1c22c61d
Roeland <roelandsteevan@gmail.com>
2020-05-24 15:40:34 +0200

    replaced test server with dating feature

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 const express = require('express')
 const ejs = require('ejs')
-const camelCase = require('camelcase')
+const camelCase = require('camelcase') // moet ik nog gebruiken
 const app = express()
 const port = 3000
-
-var profileData = {age: 20, study: 'CMD'}
-var hobbies = ['sporten', 'gamen', 'express gebruiken']
 
 app
 	.set('view engine', 'ejs') //making ejs the view engine of express
 	.use('/static', express.static('static')) //link to the static file directory
 
+	.get('/', homePage) //homepage that uses an html file
 
-//homepage that uses an html file
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+function homePage(req, res) {
+	res.render('index')//renders profile.ejs
+}
 
-//contact page that just contains text
-app.get('/contact', (req, res) => res.send('This is the contact page!'))
-
-//about page that just contains text
-app.get('/about', (req, res) => res.send('This is the about page!'));
-
-//send a mp3 file if the user goes to /mp3
-app.get('/mp3', (req, res) => res.sendFile(__dirname + '/static/tunes/cant-stop.mp3'))
-
-//acces the id in the request object to show user wich id he is using. This id is dynamic, but not saved in any database
-app.get('/account/:id', (req, res) => res.send('Your account number is: ' + '<h2>' + req.params.id + '</h2>' + '<p>Change the number by adding a number to the end of \"http://localhost:3000/account/\"</p>'));
-
-//I was curious what would happen if I linked to a font
-//spoiler: it downloads that font...
-app.get('/fonts', (req, res) => res.sendFile(__dirname + '/static/fonts/proxima_nova_light.ttf'))
-
-//you don't send ejs files, but render them
-//express knows the files are in the views folder, and knows its a ejs file. So the path is just 'profile'
-app.get('/profile/:name', (req, res) => {
-	res.render('profile', {person: req.params.name, data: profileData, hobbies: hobbies})
-})
-
-
-//handles 404 pages
 app.use(function (req, res, next) {
-  res.status(404).send('<h1>Sorry, niet deze pagina bestaat niet vriend</h1>')
+  res.status(404).send('<h1>Sorry, niet deze pagina bestaat niet vriend</h1>') //handles 404 pages
 })
-
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+// var profileData = {age: 20, study: 'CMD'}
+// var hobbies = ['sporten', 'gamen', 'express gebruiken']
+
+// app
+// 	.set('view engine', 'ejs') //making ejs the view engine of express
+// 	.use('/static', express.static('static')) //link to the static file directory
+
+// 	.get('/', (req, res) => res.sendFile(__dirname + '/static/index.html')) //homepage that uses an html file
+// 	.get('/contact', (req, res) => res.send('This is the contact page!')) //contact page that just contains text
+// 	.get('/about', (req, res) => res.send('This is the about page!')) //about page that just contains text
+// 	.get('/mp3', (req, res) => res.sendFile(__dirname + '/static/tunes/cant-stop.mp3')) //send a mp3 file if the user goes to /mp3
+// 	.get('/account/:id', (req, res) => res.send('Your account number is: ' + '<h2>' + req.params.id + '</h2>' + '<p>Change the number by adding a number to the end of \"http://localhost:3000/account/\"</p>'))
+// 	.get('/fonts', (req, res) => res.sendFile(__dirname + '/static/fonts/proxima_nova_light.ttf')) //link to font, not usefull
+// 	.get('/profile/:name', dynamicProfile);//shows dynamic profile
+
+// function dynamicProfile(req, res) {
+// 	res.render('profile', {person: req.params.name, data: profileData, hobbies: hobbies})//renders profile.ejs
+// }
+
+// app.use(function (req, res, next) {
+//   res.status(404).send('<h1>Sorry, niet deze pagina bestaat niet vriend</h1>') //handles 404 pages
+// })
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
