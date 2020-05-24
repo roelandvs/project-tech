257e1736fd95deb465e7e6c2b3ce2eba0e412fe8
Roeland <roelandsteevan@gmail.com>
2020-05-24 18:41:20 +0200

    add register pages

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ const express = require('express')
 const ejs = require('ejs')
 const camelCase = require('camelcase') // moet ik nog gebruiken
 const app = express()
+const bodyParser = require('body-parser')
+const slug = require('slug')
 const port = 3000
+
+var profileData = {age: 20, study: 'CMD'}
+var hobbies = ['sporten', 'gamen', 'express gebruiken']
 
 app
 	.set('view engine', 'ejs') //making ejs the view engine of express
 	.use('/static', express.static('static')) //link to the static file directory
+	.use(bodyParser.urlencoded({extended: true})) //enables to get data through post request
 
 	.get('/', homePage) //homepage that uses an html file
+	.get('/register', (req, res) => res.render('register')) //renders dating feature page one
+	.get('/register-two', (req, res) => res.render('register-two')) //renders dating feature page two
+	.get('/register-three', (req, res) => res.render('register-three')) //renders dating feature page two
+
+	.get('/contact', (req, res) => res.send('This is the contact page!')) //contact page that just contains text
+	.get('/about', (req, res) => res.send('This is the about page!')) //about page that just contains text
+	.get('/mp3', (req, res) => res.sendFile(__dirname + '/static/tunes/cant-stop.mp3')) //send a mp3 file if the user goes to /mp3
+	.get('/account/:id', (req, res) => res.send('Your account number is: ' + '<h2>' + req.params.id + '</h2>' + '<p>Change the number by adding a number to the end of \"http://localhost:3000/account/\"</p>'))
+	.get('/fonts', (req, res) => res.sendFile(__dirname + '/static/fonts/proxima_nova_light.ttf')) //link to font, not usefull
+	.get('/profile/:name', dynamicProfile)//shows dynamic profile
 
 function homePage(req, res) {
-	res.render('index')//renders profile.ejs
+	res.render('links')//renders links.ejs
+}
+
+function dynamicProfile(req, res) {
+	res.render('profile', {person: req.params.name, data: profileData, hobbies: hobbies})//renders profile.ejs
 }
 
 app.use(function (req, res, next) {
@@ -19,6 +39,9 @@ app.use(function (req, res, next) {
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+//These comments are the old "test server". They're here temporarily'
 
 // var profileData = {age: 20, study: 'CMD'}
 // var hobbies = ['sporten', 'gamen', 'express gebruiken']
