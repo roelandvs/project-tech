7428dcf8dac17115873046c8d41a237bae009c0b
Roeland <roelandsteevan@gmail.com>
2020-05-31 14:19:57 +0200

    start FE files

---
+++ playground/oefening-FE.js
@@ -0,0 +1,45 @@
+const animals = [
+  {
+    name: 'Hansie',
+    species: 'dog',
+    cost: 45
+  },
+  {
+    name: 'Joopie',
+    species: 'dog',
+    cost: 22
+  },
+  {
+    name: 'Beppy',
+    species: 'fish',
+    cost: 10
+  },
+  {
+    name: 'Sem',
+    species: 'fish',
+    cost: 10
+  },
+  {
+    name: 'Lolita',
+    species: 'dog',
+    cost: 60
+  }, 
+  {
+    name: 'Fluffybeans',
+    species: 'pig',
+    cost: 20
+  },
+  {
+    name: 'BearFluf',
+    species: 'pig',
+    cost: 15
+  },
+  {
+    name: 'Manke Neel',
+    species: 'dog',
+    cost: 1
+  }
+];
+
+let dogs = [];
+
---
+++ static/js/javascript.js
@@ -0,0 +1,5 @@
+// Selecteer fieldsets
+// Maak fieldsets onzichtbaar
+// selecteer 1 fieldset en maak deze niet onzichtbaar
+// selecteer next button
+// door te klikken op button wordt de nieuwe fieldset zichtbaar
