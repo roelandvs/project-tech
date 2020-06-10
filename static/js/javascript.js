e8bed6e8c1ff0435b2c45f336c6b1b2013d9739e
Roeland <roelandsteevan@gmail.com>
2020-06-10 16:58:47 +0200

    add js progressive disclosure

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,5 +1,58 @@
-// Selecteer fieldsets
-// Maak fieldsets onzichtbaar
-// selecteer 1 fieldset en maak deze niet onzichtbaar
-// selecteer next button
-// door te klikken op button wordt de nieuwe fieldset zichtbaar
+let fieldset = document.body.getElementsByTagName('fieldset');
+// let nextButton = document.body.getElementsByClassName('btn-next-container');
+let button = document.body.getElementsByClassName('btn-next');
+let previousButton = document.body.getElementsByClassName('btn-previous');
+let counter = 0;
+
+showContent(); //makes all fieldsets invisible, except first one
+
+function showContent() {
+	console.log(fieldset, counter);
+	if (counter === 0) {
+		for (let i = 0; i < fieldset.length; i++) {
+			fieldset[i].classList.add('makeInvisible');
+		}
+		fieldset[counter].classList.replace('makeInvisible', 'addVisibility');
+	} else if (counter > fieldset.length - 1){
+		console.log(einde);
+	} else {
+		fieldset[counter].classList.replace('makeInvisible', 'addVisibility');
+		fieldset[counter - 1].classList.replace('addVisibility', 'makeInvisible');
+	}
+}
+
+function previousContent() {
+	console.log(fieldset, counter);
+	fieldset[counter + 1].classList.replace('addVisibility', 'makeInvisible');
+	fieldset[counter].classList.replace('makeInvisible', 'addVisibility');
+}
+
+function addCounter (){
+	let input = fieldset[counter].getElementsByTagName('input');
+	let valid = true;
+
+	for (let i = 0; i < input.length; i++) {
+		if (input[i].value == false) {
+			input[i].classList.add('invalid');
+			valid = false;
+		}
+	}
+
+	if (valid === true) {
+		counter += 1;
+		showContent();
+	}
+}
+
+function subtractCounter (){
+	counter -= 1;
+	previousContent();
+}
+
+for (let i = 0; i < fieldset.length; i++) {
+	button[i].addEventListener('click', addCounter);
+}
+
+for (let i = 0; i < previousButton.length; i++) {
+	previousButton[i].addEventListener('click', subtractCounter);
+}
