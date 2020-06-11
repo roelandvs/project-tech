e1fc2bfb89f94d3f4073b60007401d9a732780d2
Roeland <roelandsteevan@gmail.com>
2020-06-11 16:16:40 +0200

    js form validation working

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -2,50 +2,116 @@ let fieldset = document.body.getElementsByTagName('fieldset');
 // let nextButton = document.body.getElementsByClassName('btn-next-container');
 let button = document.body.getElementsByClassName('btn-next');
 let previousButton = document.body.getElementsByClassName('btn-previous');
-let counter = 0;
+let activeFieldset = 0;
 
 showContent(); //makes all fieldsets invisible, except first one
 
 function showContent() {
-	console.log(fieldset, counter);
-	if (counter === 0) {
+	if (activeFieldset === 0) {
 		for (let i = 0; i < fieldset.length; i++) {
 			fieldset[i].classList.add('makeInvisible');
 		}
-		fieldset[counter].classList.replace('makeInvisible', 'addVisibility');
-	} else if (counter > fieldset.length - 1){
-		console.log(einde);
+		fieldset[activeFieldset].classList.replace('makeInvisible', 'addVisibility');
+	} else if (activeFieldset > fieldset.length - 1){
+		console.log('einde');
 	} else {
-		fieldset[counter].classList.replace('makeInvisible', 'addVisibility');
-		fieldset[counter - 1].classList.replace('addVisibility', 'makeInvisible');
+		fieldset[activeFieldset].classList.replace('makeInvisible', 'addVisibility');
+		fieldset[activeFieldset - 1].classList.replace('addVisibility', 'makeInvisible');
 	}
 }
 
 function previousContent() {
-	console.log(fieldset, counter);
-	fieldset[counter + 1].classList.replace('addVisibility', 'makeInvisible');
-	fieldset[counter].classList.replace('makeInvisible', 'addVisibility');
+	fieldset[activeFieldset + 1].classList.replace('addVisibility', 'makeInvisible');
+	fieldset[activeFieldset].classList.replace('makeInvisible', 'addVisibility');
 }
 
-function addCounter (){
-	let input = fieldset[counter].getElementsByTagName('input');
+function addCounter() {
+	let input = fieldset[activeFieldset].getElementsByTagName('input'); //gets the inputs of the active Fieldset
 	let valid = true;
+	let activeGender = []; //in higher scope because it must save the array
+	let activePreference = []; //in higher scope because it must save the array
 
 	for (let i = 0; i < input.length; i++) {
-		if (input[i].value == false) {
-			input[i].classList.add('invalid');
-			valid = false;
-		}
+ 
+		//switch selects wich cases it should run based on the attribute names that match input[i].getAttribute("name")
+	    switch (input[i].getAttribute("name")) {
+	        case "firstName":
+	            if (input[i].value.length < 2) { //checks if input is 2 characters of more
+	                input[i].classList.add('invalid');
+	                valid = false;
+	            }
+	            break;
+
+	        case "email":
+	            if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input[i].value) == false) { //checks if the input is equal to the RegEx objects definiton of an emailaddress
+	                input[i].classList.add('invalid');
+	                valid = false;
+	            }
+	            break;
+
+	        case "password":
+	            if (input[i].value == false) {
+	                input[i].classList.add('invalid');
+	                valid = false;
+	            }
+	            break;
+
+	        case "birthday":
+	            if (input[i].value == false) {
+	                input[i].classList.add('invalid');
+	                valid = false;
+	            }
+	            break;
+
+	        case "gender":
+	        	let gender = input[i].checked; //checks if input is true or false
+	        	if (gender == false) {
+	        		activeGender.push(gender); //push "false" to activeGender
+	        	}
+
+	            if (activeGender.length == 2) { //if both inputs are false there is no input
+	                input[i].classList.add('invalid');
+	                valid = false;
+	            }
+	            break;
+
+	        case "preference":
+	            let preference = input[i].checked;
+	        	if (preference == false) {
+	        		activePreference.push(preference);
+	        	}
+
+	            if (activePreference.length == 2) {
+	                input[i].classList.add('invalid');
+	                valid = false;
+	            }
+	            break;
+
+    		case "profilePicture":
+    			if (input[i].value == false) {
+	                input[i].classList.add('invalid');
+	                valid = false;
+	            }
+	            break;
+
+       		case "description":
+    			if (input[i].value == false) {
+	                input[i].classList.add('invalid');
+	                valid = false;
+	            }
+	            break;
+	    }
+
 	}
 
 	if (valid === true) {
-		counter += 1;
+		activeFieldset += 1;
 		showContent();
 	}
 }
 
 function subtractCounter (){
-	counter -= 1;
+	activeFieldset -= 1;
 	previousContent();
 }
 
@@ -56,3 +122,4 @@ for (let i = 0; i < fieldset.length; i++) {
 for (let i = 0; i < previousButton.length; i++) {
 	previousButton[i].addEventListener('click', subtractCounter);
 }
+
